c1786c0da5e67c723083115f3d422c9d4a3a2762
Claudio Perroni <c.perroni@ifortech.com>
2024-02-16 09:22:55 +0100

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
@@ -29,6 +30,7 @@ export default function RootLayout({
           {children}
           <Footer />
           <Toaster />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
